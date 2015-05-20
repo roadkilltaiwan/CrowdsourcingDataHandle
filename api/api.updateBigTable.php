229ec1928033f2ed4df7e4ffdb7f8fca8bcb95bf
trashmai <trashmai@gmail.com>
2015-05-20 15:13:47 +0800

    using pdo instead of mysql_*

--- api/api.updateBigTable.php
+++ api/api.updateBigTable.php
@@ -17,118 +17,136 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+<?php
+
 $dir_api = implode("/", explode("/", realpath(__FILE__), -1));
 
 require_once $dir_api . "/../includes/system.inc";
 require_once $dir_api . "/../conf/db_constant.inc.php";
 
 
-$db = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
-mysql_select_db(DB_DATABASE, $db);
+$dbh = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';charset=utf8', DB_USER, DB_PASS);
+$dbh->query("set names 'utf8';");
 
-$sql = "set names 'utf8';";
-mysql_query($sql);
+// $db = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+// mysql_select_db(DB_DATABASE, $db);
+
+// $sql = "set names 'utf8';";
+// mysql_query($sql);
 
 $sets = array();
+$vals = array();
 if (!empty($_REQUEST)) {
-#	var_dump($_REQUEST);
+#       var_dump($_REQUEST);
 
-	if ($_REQUEST['inWhiteList'] ==	'true') {
-		$_REQUEST['tagged'] = 'true';
-	}
+        if ($_REQUEST['inWhiteList'] == 'true') {
+                $_REQUEST['tagged'] = 'true';
+        }
 
-	foreach ($_REQUEST as $col => $val) {
-		if ($val == 'true') {
-			$val = 1;
-		}
-		else if ($val == 'false') {
-			$val = 0;
-		}
-		if (($col != 'cname1')&&($col != 'cname2')&&($col != 'cname3')&&($col != 'sname1')&&($col != 'sname2')&&($col != 'sname3')&&($col != 'spid')&&($col != 'coid')) {
-			if (($col != 'x')&&($col != 'y')&&($col != 'altitude')) {
-				$sets[] = "`$col` = '".mysql_real_escape_string($val)."'";
-			}
-			else if ($col == 'x') {
-				if (!empty($val)) {
-					$sets[] = "`$col` = '".mysql_real_escape_string($val)."'";
-				}
-			}
-			else if ($col == 'y') {
-				if (!empty($val)) {
-					$sets[] = "`$col` = '".mysql_real_escape_string($val)."'";
-				}
-			}
-			else if ($col == 'altitude') {
-				if (!empty($val)) {
-					$sets[] = "`$col` = '".mysql_real_escape_string($val)."'";
-				}
-			}
-		}
-	}
+        file_put_contents("req.log", var_export($_REQUEST, true), FILE_APPEND);
 
-	$cnames = $_REQUEST['cname1'] . "|" . $_REQUEST['cname2'] . "|" . $_REQUEST['cname3'];
-	$snames = $_REQUEST['sname1'] . "|" . $_REQUEST['sname2'] . "|" . $_REQUEST['sname3'];
+        foreach ($_REQUEST as $col => $val) {
+                if ($val == 'true') {
+                        $val = 1;
+                }
+                else if ($val == 'false') {
+                        $val = 0;
+                }
+                if (($col != 'cname1')&&($col != 'cname2')&&($col != 'cname3')&&($col != 'sname1')&&($col != 'sname2')&&($col != 'sname3')&&($col != 'spid')&&($col != 'coid')) {
+                        if (($col != 'x')&&($col != 'y')&&($col != 'altitude')) {
+                                $sets[] = "`$col` = ?";
+                                $vals[] = $val;
+                        }
+                        else if ($col == 'x') {
+                                if (!empty($val)) {
+                                        $sets[] = "`$col` = ?";
+                                        $vals[] = $val;
+                                }
+                        }
+                        else if ($col == 'y') {
+                                if (!empty($val)) {
+                                        $sets[] = "`$col` = ?";
+                                        $vals[] = $val;
+                                }
+                        }
+                        else if ($col == 'altitude') {
+                                if (!empty($val)) {
+                                        $sets[] = "`$col` = ?";
+                                        $vals[] = $val;
+                                }
+                        }
+                }
+        }
 
-	$cnames = trim($cnames, "| ");
-	$cnames = str_replace("||", "|", $cnames);
+        file_put_contents("req.log", var_export($sets, true), FILE_APPEND);
 
-	$snames = trim($snames, "| ");
-	$snames = str_replace("||", "|", $snames);
+        $cnames = $_REQUEST['cname1'] . "|" . $_REQUEST['cname2'] . "|" . $_REQUEST['cname3'];
+        $snames = $_REQUEST['sname1'] . "|" . $_REQUEST['sname2'] . "|" . $_REQUEST['sname3'];
 
-	$sets[] = "`common_name` = '".mysql_real_escape_string($cnames)."'";
-	$sets[] = "`canonical_name` = '".mysql_real_escape_string($snames)."'";
+        $cnames = trim($cnames, "| ");
+        $cnames = str_replace("||", "|", $cnames);
 
-	$ids = array();
-	$spids = array();
-	$coids = array();
-	if (!empty($_REQUEST['spid'])) {
-		$spids = explode("|", $_REQUEST['spid']);
-	}
-	foreach ($spids as $spid) {
-		if (!empty($spid)) {
-			$ids["SpecimenID($spid)"] = true;
-		}
-	}
+        $snames = trim($snames, "| ");
+        $snames = str_replace("||", "|", $snames);
 
-	if (!empty($_REQUEST['coid'])) {
-		$coids = explode("|", $_REQUEST['coid']);
-	}
-	foreach ($coids as $coid) {
-		if (!empty($coid)) {
-			$ids["CollectionID($coid)"] = true;
-		}
-	}
-	$id_string = "";
-	if (!empty($ids)) {
-		$id_string = implode("|", array_keys($ids));
-	}
+        $sets[] = "`common_name` = ?";
+        $vals[] = $cnames;
+        $sets[] = "`canonical_name` = ?";
+        $vals[] = $snames;
 
-	$sets[] = "`custom_id` = '".mysql_real_escape_string($id_string)."'";
+        $ids = array();
+        $spids = array();
+        $coids = array();
+        if (!empty($_REQUEST['spid'])) {
+                $spids = explode("|", $_REQUEST['spid']);
+        }
+        foreach ($spids as $spid) {
+                if (!empty($spid)) {
+                        $ids["SpecimenID($spid)"] = true;
+                }
+        }
+
+        if (!empty($_REQUEST['coid'])) {
+                $coids = explode("|", $_REQUEST['coid']);
+        }
+        foreach ($coids as $coid) {
+                if (!empty($coid)) {
+                        $ids["CollectionID($coid)"] = true;
+                }
+        }
+        $id_string = "";
+        if (!empty($ids)) {
+                $id_string = implode("|", array_keys($ids));
+        }
+
+        $sets[] = "`custom_id` = ?";
+        $vals[] = $id_string;
 
 
-	$sql = "replace into bigTable set " . implode(", ", $sets);
-	$res = mysql_query($sql);
+        $sql = "replace into bigTable set " . implode(", ", $sets);
+        // $res = mysql_query($sql);
+        $query = $dbh->prepare($sql);
+        $res = $query->execute($vals);
 
-	//存照片
-	$photo_id = $_REQUEST['photo_id'];
-	$image = $_REQUEST['picture'];
-	$cmd_tpl = "/usr/bin/php $dir_api/../utils/downloadFBImage.php \"%s\" \"%s\" > /dev/null 2>/dev/null &";
-	$cmd = sprintf($cmd_tpl, $image, $photo_id);
-	$f = exec($cmd, $o);
+        //存照片
+        $photo_id = $_REQUEST['photo_id'];
+        $image = $_REQUEST['picture'];
+        $cmd_tpl = "/usr/bin/php $dir_api/../utils/downloadFBImage.php \"%s\" \"%s\" > /dev/null 2>/dev/null &";
+        $cmd = sprintf($cmd_tpl, $image, $photo_id);
+        $f = exec($cmd, $o);
 
-	if ($res == false) {
-		echo json_encode(array('state'=>false, 'SQL'=>$sql, 'save_img' => $cmd, 'o'=>$o));
-	}
-	else {
-		echo json_encode(array('state'=>true, 'SQL'=>$sql, 'save_img' => $cmd, 'o'=>$o));
-	}
+        if ($res == false) {
+                echo json_encode(array('state'=>false, 'SQL'=>$sql, 'save_img' => $cmd, 'o'=>$o));
+        }
+        else {
+                echo json_encode(array('state'=>true, 'SQL'=>$sql, 'save_img' => $cmd, 'o'=>$o));
+        }
 
 
 
 }
 else {
-	echo json_encode(array('state'=>false, 'SQL'=>''));
+        echo json_encode(array('state'=>false, 'SQL'=>''));
 }
-
 
 ?>
